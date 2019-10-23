aeb32446658a2ce2bb8cd58f2f7cdb4dedf62b33
Vitaliy <55364824+OpLuK@users.noreply.github.com>
2019-10-23 12:21:58 +0300

    Add files via upload

--- Array min\max (random)/src/class1.java
+++ Array min\max (random)/src/class1.java
@@ -1,5 +1,7 @@
 public class class1 {
 	public static void main (String [] args) {
+		float arifm;
+		int sum = 0;
 		int i;
 		int [] MyArray = new int [10];
 		for (i = 0; i < 10; i = i + 1) {
@@ -12,15 +14,18 @@ public class class1 {
 		System.out.println("]");
 		int max = MyArray [0];
 		int min = MyArray[0];
-		for (i = 0; i <= 9; i = i + 1) {
+		for (i = 0; i < 10; i = i + 1) {
 			if (MyArray[i] > max) {
 				max = MyArray [i];
 			}
 			if (MyArray[i] < min) {
 				min = MyArray[i];
 			}
+			sum = sum + MyArray[i];
 		}
+		arifm = sum / MyArray.length;
 		System.out.println("Наибольший элемент массива равен: " + max);
 		System.out.println("Наименьший элемент массива равен: " + min);
+		System.out.println("Среднее арифметическое равно: " + arifm);
 	}
 }
